1dc318e04a8accda7638db03c01493940128fbb9
h7k2 <keddousheytem@gmail.com>
2025-08-08 17:11:28 +0200

    Refine bubble_sort function documentation and improve input validation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,35 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array using Bubble Sort
- * @array: pointer to the array
- * @size: number of elements
- *
- * Return: nothing
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The number of elements in the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int tmp;
-    int swapped;
+	size_t i, j;
+	int tmp;
+	int swapped;
 
-    if (size <= 1)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size; i++)
-    {
-        swapped = 0;
-
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-
-        if (swapped == 0)
-            break;
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
