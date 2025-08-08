75afa7c2258a2a31480a426f4479d9b9798e6dcf
h7k2 <keddousheytem@gmail.com>
2025-08-08 17:34:26 +0200

    Add quick_sort implementation with Lomuto partitioning

---
+++ 3-quick_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers using Quick sort
+ * @array: The array to sort
+ * @size: Number of elements in array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
+
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
+}
+
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low;
+	int j, temp;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+			i++;
+		}
+	}
+	temp = array[i];
+	array[i] = array[high];
+	array[high] = temp;
+	print_array(array, size);
+	return (i);
+}
