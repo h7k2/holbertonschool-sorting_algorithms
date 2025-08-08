a88efc33fdb8464ba2746927d63937126145daa7
h7k2 <keddousheytem@gmail.com>
2025-08-08 17:36:24 +0200

    Refactor quick_sort and related functions for clarity and consistency in documentation

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sorts an array of integers using Quick sort
+ * quick_sort - Sorts an array of integers in ascending order
+ *              using the Quick sort algorithm (Lomuto scheme)
  * @array: The array to sort
- * @size: Number of elements in array
+ * @size: The number of elements in the array
  */
 void quick_sort(int *array, size_t size)
 {
@@ -13,39 +14,60 @@ void quick_sort(int *array, size_t size)
 	quick_sort_recursive(array, 0, size - 1, size);
 }
 
-void quick_sort_recursive(int *array, int low, int high, size_t size)
+/**
+ * quick_sort_recursive - Recursively applies Quick sort
+ * @array: The array to sort
+ * @low: Start index
+ * @high: End index
+ * @size: Total size of the array (for printing)
+ */
+static void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
+	int pivot;
+
 	if (low < high)
 	{
-		int pivot = lomuto_partition(array, low, high, size);
-
+		pivot = lomuto_partition(array, low, high, size);
 		quick_sort_recursive(array, low, pivot - 1, size);
 		quick_sort_recursive(array, pivot + 1, high, size);
 	}
 }
 
-int lomuto_partition(int *array, int low, int high, size_t size)
+/**
+ * lomuto_partition - Partitions the array using Lomuto scheme
+ * @array: The array to partition
+ * @low: Start index
+ * @high: End index (pivot)
+ * @size: Size of the array (for printing)
+ *
+ * Return: The pivot index after partitioning
+ */
+static int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int i = low - 1;
-	int j, temp;
+	int j, tmp;
 
 	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, size);
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
 		}
 	}
-
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	print_array(array, size);
-
+	if (array[i + 1] != array[high])
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
 	return (i + 1);
 }
