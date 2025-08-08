1a374d035d8d3090e8ba10aa87a9b22a9170e1e4
h7k2 <keddousheytem@gmail.com>
2025-08-08 17:27:06 +0200

    Refactor insertion_sort_list to improve readability and maintain functionality

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,23 +6,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp;
+	listint_t *current, *prev;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
 	current = (*list)->next;
-
 	while (current)
 	{
-		tmp = current->prev;
-
-		while (tmp && tmp->n > current->n)
+		prev = current->prev;
+		while (prev && current->n < prev->n)
 		{
-			int swap = tmp->n;
-			((int *)&tmp->n)[0] = current->n;
-			((int *)&current->n)[0] = swap;
+			if (current->next)
+				current->next->prev = prev;
+			prev->next = current->next;
+
+			current->prev = prev->prev;
+			current->next = prev;
+
+			if (prev->prev)
+				prev->prev->next = current;
+			prev->prev = current;
+
+			if (current->prev == NULL)
+				*list = current;
 
 			print_list(*list);
-
-			tmp = tmp->prev;
+			prev = current->prev;
 		}
 		current = current->next;
 	}
