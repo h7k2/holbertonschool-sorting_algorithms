36a0acfba5a7a9c8028723a18009672454f39111
h7k2 <keddousheytem@gmail.com>
2025-08-08 17:35:38 +0200

    Fix quick_sort function to handle NULL array input and correct partitioning logic

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,7 +7,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	quick_sort_recursive(array, 0, size - 1, size);
@@ -27,23 +27,25 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low;
+	int i = low - 1;
 	int j, temp;
 
 	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
+			i++;
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
 			print_array(array, size);
-			i++;
 		}
 	}
-	temp = array[i];
-	array[i] = array[high];
+
+	temp = array[i + 1];
+	array[i + 1] = array[high];
 	array[high] = temp;
 	print_array(array, size);
-	return (i);
+
+	return (i + 1);
 }
